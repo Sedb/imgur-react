6f002afddceebfd8f3827fdbfdc65a427e229b86
Juan Gallisa <j.s.gallisa@gmail.com>
2015-09-23 20:21:34 -0400

    Added topics to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
 var Router = require('react-router');
+var Reflux = require('reflux');
+var TopicStore = require('../stores/topic-store');
+var Actions = require('../actions');
 var Link = Router.Link;
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+  getInitialState: function() {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function() {
+    Actions.getTopics();
+  },
   render: function() {
     return <nav className="navbar navbar-inverse navbar-fixed-top header">
       <div className="container-fluid">
@@ -10,8 +24,21 @@ module.exports = React.createClass({
           Imgur React
         </Link>
         <ul className="nav navbar-nav navbar-right">
+          {this.renderTopics()}
         </ul>
       </div>
     </nav>
+  },
+  renderTopics: function(){
+    return this.state.topics.map(function(topic){
+      return <li key={topic.id}>
+        <Link activeClassName="active" to={"topics/"+topic.id}>
+          {topic.name}
+        </Link>
+      </li>
+    });
+  },
+  onChange: function(event, topics){
+    this.setState({topics: topics});
   }
 });
