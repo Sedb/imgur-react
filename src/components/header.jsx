ef2e331b25d1ba9d6d722e22f952f907c3d308af
Juan Gallisa <j.s.gallisa@gmail.com>
2015-09-23 20:15:21 -0400

    Fetched topics from imgur and created a list of topics

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,14 @@ var Router = require('react-router');
 var Link = Router.Link;
 
 module.exports = React.createClass({
-    render: function() {
-    return <nav className="navbar navbar-inverse header">
+  render: function() {
+    return <nav className="navbar navbar-inverse navbar-fixed-top header">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand">
           Imgur React
         </Link>
+        <ul className="nav navbar-nav navbar-right">
+        </ul>
       </div>
     </nav>
   }
